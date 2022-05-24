c01c11b123e8509dbe60dd570b8521780a2eef36
NizeNize2K <nizeimbaboy.2@gmail.com>
2022-05-24 22:17:36 +0700

    update scirpt2

--- connext-version-update-by-nongnicelnwza007.sh
+++ connext-version-update-by-nongnicelnwza007.sh
@@ -3,10 +3,11 @@
 
 echo ------------Connext Update Node Versoin Start!!!------------
 cd /root/nxtp-router-docker-compose
+echo You ROUTER_VERSION 
 cat .env | grep ROUTER_VERSION
 
-read -p "Now connext versoin: " connextcurrent
-read -p "Update connext router To versoin: " connextnew
+read -p "Now connext router version: " connextcurrent
+read -p "Update connext router To version: " connextnew
 
 
 sed -i "s/${connextcurrent}/${connextnew}/g" .env
