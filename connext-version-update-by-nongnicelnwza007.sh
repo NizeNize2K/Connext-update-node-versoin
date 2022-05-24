5e7f52dbecf50bef2377b31e065686a6fcc88e1f
NizeNize2K <nizeimbaboy.2@gmail.com>
2022-05-24 22:07:41 +0700

    add scirpt

---
+++ connext-version-update-by-nongnicelnwza007.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#set -x
+cd /root/nxtp-router-docker-compose
+cat .env | grep ROUTER_VERSION
+
+read -p "Now connext versoin: " connextcurrent
+read -p "Update connext router To versoin: " connextnew
+
+
+sed -i "s/${connextcurrent}/${connextnew}/g" .env
+
+
+
+docker-compose down
+sleep 2
+docker-compose pull
+sleep 2
+docker-compose up -d
+
+
+echo Completed Update to Version: $connextnew!!!!
