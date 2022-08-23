d9c34bfd60cb3b4edaf58f2426f176bbea277eec
ActCool969 <actcool2932@gmail.com>
2022-08-23 10:59:05 +0700

    Delete File R2.3

--- test_git_script_transformation.sql
+++
@@ -1,3 +0,0 @@
-select 'transformation' from table_test;
-
---(n) plot, See also: outline, story, scenario, subject, Syn. เรื่องราว, Example: ภาพยนตร์มีเนื้อเรื่องยกย่องวีรกรรมการเสียสละเพื่อชาติ, Thai Definition: สาระของเรื่อง ไทย-ไทย: พจนานุกรมฉบับราชบัณฑิตยสถาน พ.ศ. ๒๕๕๔ เนื้อเรื่อง
